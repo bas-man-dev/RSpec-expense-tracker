4dccd7c5ac0f3f3cbe47ecf7bf1a05452b7f20c3
Bas Man <bas.man.developer@gmail.com>
2023-05-14 17:46:57 +0100

    written new happy/unhappy tests for GET, ...TODO

--- expense_tracker/spec/unit/app/api_spec.rb
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -5,7 +5,7 @@ module ExpenseTracker
   RSpec.describe API do
     include Rack::Test::Methods
 
-    def app 
+    def app
       API.new(ledger: ledger)
     end
 
@@ -13,7 +13,6 @@ module ExpenseTracker
     describe 'POST /expenses' do
       context 'when the expense is succesfully recorded' do
         let(:expense) { { 'some' => 'data' } }
-
         before do
           allow(ledger).to receive(:record)
             .with(expense)
@@ -25,15 +24,14 @@ module ExpenseTracker
           parsed = JSON.parse(last_response.body)
           expect(parsed).to include('expense_id' => 417)
         end
-
         it 'responds with a 200 (OK)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(200)
         end
       end
+
       context 'when the expense fails validation' do
         let(:expense) { { 'some' => 'data' } }
-
         before do
           allow(ledger).to receive(:record)
             .with(expense)
@@ -45,11 +43,21 @@ module ExpenseTracker
           parsed = JSON.parse(last_response.body)
           expect(parsed).to include('error' => 'Expense Incomplete')
         end
-
         it 'responds with a 422 (Unprocessable entity)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(422)
         end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on the given date' do
+        it 'returns the expense records as JSON'
+        it 'responds with a 200 (OK)'
+      end
+      context 'when there are no expenses on the given date' do
+        it 'returns an empty array as JSON'
+        it 'responds with a 200 (OK)'
       end
     end
   end
