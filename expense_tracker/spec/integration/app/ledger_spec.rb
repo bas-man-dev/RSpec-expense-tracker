4562d4c58bc06ab9336d17ec2e4eaf8dbeced921
Bas Man <bas.man.developer@gmail.com>
2023-05-29 19:54:51 +0100

    possible to make API calls now and access when server running

--- expense_tracker/spec/integration/app/ledger_spec.rb
+++ expense_tracker/spec/integration/app/ledger_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../../../app/ledger'
-require_relative '../../../config/sequel'
-
 
 module ExpenseTracker
   RSpec.describe Ledger, :aggregate_failures, :db do
@@ -38,11 +36,27 @@ module ExpenseTracker
 
           expect(result).not_to be_success
           expect(result.expense_id).to eq(nil)
-          expect(result.error_message).to include('`payee`` is required')
+          expect(result.error_message).to include('`payee` is required')
 
           expect(DB[:expenses].count).to eq(0)
         end
       end
     end
+    describe '#expenses_on' do
+      it 'returns all expenses for the provided date' do
+        result_1 = ledger.record(expense.merge('date' => '2017-06-10'))
+        result_2 = ledger.record(expense.merge('date' => '2017-06-10'))
+        result_3 = ledger.record(expense.merge('date' => '2017-06-11'))
+
+        expect(ledger.expenses_on('2017-06-10')).to contain_exactly(
+          a_hash_including(id: result_1.expense_id),
+          a_hash_including(id: result_2.expense_id))
+      end
+
+      it 'returns as blank array when there are no matching expenses' do
+        expect(ledger.expenses_on(2017-06-10)).to eq([])
+      end
+    end
+
   end
 end
